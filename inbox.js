b30cf97afdbe24348a0433f1823658045339ec3d
Nick Barrett <nickbarrett@protonmail.com>
2020-09-04 13:47:13 -0500

    Update inbox.js

--- inbox.js
+++ inbox.js
@@ -3,7 +3,7 @@
 // raw javascript
 
 if (location.hash[1] == 'l') {
-  window.location.href='https://mail.google.com/mail/u/0/#search/label%3A'+location.hash.replace("#label/","").toLowerCase().concat('+is%3Ainbox')
+  window.location.href='https://mail.google.com/mail/u/0/#search/'+location.hash.replace("#label/","label%3A").toLowerCase().concat('+is%3Ainbox')
 } else {
   window.location.href=location.href.concat('+is%3Ainbox')
 }
@@ -11,4 +11,4 @@ if (location.hash[1] == 'l') {
 
 // converted on https://mrcoles.com/bookmarklet/
 
-javascript:(function()%7Bif%20(location.hash%5B1%5D%20%3D%20'l')%20%7Bwindow.location.href%3D'https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F%23search%2Flabel%253A'%2Blocation.hash.replace(%22%23label%2F%22%2C%22%22).toLowerCase().concat('%2Bis%253Ainbox')%7D%20else%20%7Bwindow.location.href%3Dlocation.href.concat('%2Bis%253Ainbox')%7D%7D)()
+javascript:(function()%7Bif%20(location.hash%5B1%5D%20%3D%3D%20'l')%20%7Bwindow.location.href%3D'https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F%23search%2F'%2Blocation.hash.replace(%22%23label%2F%22%2C%22label%253A%22).toLowerCase().concat('%2Bis%253Ainbox')%7D%20else%20%7Bwindow.location.href%3Dlocation.href.concat('%2Bis%253Ainbox')%7D%7D)()
