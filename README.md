5159ada911b41cd086491b1552b9ace322ed42c8
Nick Barrett <nickbarrett@protonmail.com>
2020-09-05 10:46:23 -0500

    v2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bookmarklets
+
+
+```javascript
+window.location.href='https://mail.google.com/mail/u/0/#search/'+location.hash.replace("/","%3A").replace("#search%3A","").replace("#imp","is%3Aimportant").replace("#category%3A","category%3A").replace("#label","label").replace("#","in%3A").toLowerCase().concat('+is%3Ainbox')
+```
+
+
+```javascript
+window.location.href='https://mail.google.com/mail/u/0/#search/'+location.hash.replace("/","%3A").replace("#search%3A","").replace("#imp","is%3Aimportant").replace("#category%3A","category%3A").replace("#label","label").replace("#","in%3A").toLowerCase().concat('+is%3Aunread')
+```
+<!-- __Focusable Button|Follow|<Optional-ID>__ -->
--- inbox.js
+++ inbox.js
@@ -1,14 +1,7 @@
 // gets the label thats currently selected and changes to search and adds is:inbox to the search.
 
 // raw javascript
-
-if (location.hash[1] == 'l') {
-  window.location.href='https://mail.google.com/mail/u/0/#search/'+location.hash.replace("#label/","label%3A").toLowerCase().concat('+is%3Ainbox')
-} else {
-  window.location.href=location.href.concat('+is%3Ainbox')
-}
-
+window.location.href='https://mail.google.com/mail/u/0/#search/'+location.hash.replace("/","%3A").replace("#search%3A","").replace("#","").toLowerCase().concat('+is%3Ainbox')
 
 // converted on https://mrcoles.com/bookmarklet/
-
 javascript:(function()%7Bif%20(location.hash%5B1%5D%20%3D%3D%20'l')%20%7Bwindow.location.href%3D'https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F%23search%2F'%2Blocation.hash.replace(%22%23label%2F%22%2C%22label%253A%22).toLowerCase().concat('%2Bis%253Ainbox')%7D%20else%20%7Bwindow.location.href%3Dlocation.href.concat('%2Bis%253Ainbox')%7D%7D)()
